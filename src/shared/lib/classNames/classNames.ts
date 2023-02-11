cfe5fe554c3b45f9b3cdefc7ecd66c20fddd176c
intergalact1c <20theuniverse@gmail.com>
2023-02-11 20:03:58 +0300

    Jest

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,9 @@
 type Mods = Record<string, boolean | string>
 
-export function classNames(cls: string, mods?: Mods, additional?: string[]): string {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [
         cls,
-        ...Object.entries(mods).filter(([__, value]) => Boolean(value)).map(([className, __]) => className),
         ...additional.filter(Boolean),
+        ...Object.entries(mods).filter(([_, value]) => Boolean(value)).map(([className, _]) => className),
     ].join(' ');
 }
