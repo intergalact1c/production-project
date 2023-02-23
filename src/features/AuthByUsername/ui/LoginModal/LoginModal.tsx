037cebc502b8e269244f62cdd08a687481df7a49
intergalact1c <20theuniverse@gmail.com>
2023-02-23 16:34:16 +0300

    remove loginModal scss file

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
-import cls from './LoginModal.module.scss';
 
 interface LoginModalProps {
     classname?: string;
@@ -15,7 +14,7 @@ export const LoginModal = ({ classname, isOpen, onClose }: LoginModalProps) => (
         lazy
         isOpen={isOpen}
         onClose={onClose}
-        classname={classNames(cls.LoginModal, {}, [classname])}
+        classname={classNames('', {}, [classname])}
     >
         <LoginForm />
     </Modal>
